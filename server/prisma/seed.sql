4b762e8a276377870bc06e836cf98e312ec85c86
pxlman <agxgv7573@gmail.com>
2026-07-12 16:47:45 +0300

    fix(docker): adding the server and auto seeding fixing in the db and server

--- server/prisma/seed.sql
+++ server/prisma/seed.sql
@@ -1,6 +1,8 @@
 -- Seed users (passwords hashed with bcrypt, agent123 and admin123)
 INSERT INTO "user" (email, phone_number, "password_hash", "name", "role")
 VALUES 
-  ('agent', '123-456-7890', '$2b$10$eUnSYctw2bA5lcDxvHyqOu4Bemi6NIQv0wTUJcIsr53t7fKi0SlOy', 'Agent Smith', 'user'),
-  ('admin', '', '$2b$10$7.mXiBV57u05riDryVUTK.E9DqYtFMUM9i3HS6P2VTmaULAl1v8fm', 'Admin User', 'admin')
+  ('agent1', '123-456-7890', '$2b$10$eUnSYctw2bA5lcDxvHyqOu4Bemi6NIQv0wTUJcIsr53t7fKi0SlOy', 'Agent Smith 1', 'user'),
+  ('agent2', '124-456-7890', '$2b$10$eUnSYctw2bA5lcDxvHyqOu4Bemi6NIQv0wTUJcIsr53t7fKi0SlOy', 'Agent Smith 2', 'user'),
+  ('admin1', '12093', '$2b$10$7.mXiBV57u05riDryVUTK.E9DqYtFMUM9i3HS6P2VTmaULAl1v8fm', 'Admin User 1', 'admin'),
+  ('admin2', '12094', '$2b$10$7.mXiBV57u05riDryVUTK.E9DqYtFMUM9i3HS6P2VTmaULAl1v8fm', 'Admin User 2', 'admin')
 ON CONFLICT (email) DO NOTHING;
