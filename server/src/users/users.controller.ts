b17d1b12cfa2b7c3557198da3fc04bb8155d64f9
pxlman <agxgv7573@gmail.com>
2026-07-16 15:33:20 +0300

    refactor: change the delete method for deactivation to be patch

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -66,7 +66,7 @@ export class UsersController {
     return this.usersService.update(userId, dto);
   }
 
-  @Delete(':userId/deactivate')
+  @Patch(':userId/deactivate')
   @UseGuards(RolesGuard)
   @Roles('admin')
   @HttpCode(HttpStatus.OK)
