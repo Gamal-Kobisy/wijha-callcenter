142867004de71a5bcfc4f100da496480e0008039
pxlman <agxgv7573@gmail.com>
2026-08-12 18:25:21 +0300

    fix: simplify ON CONFLICT clause in seed.sql

--- server/prisma/seed.sql
+++ server/prisma/seed.sql
@@ -5,4 +5,4 @@ VALUES
   ('agent2@gmail.com', '124-456-7890', '$2b$10$eUnSYctw2bA5lcDxvHyqOu4Bemi6NIQv0wTUJcIsr53t7fKi0SlOy', 'Agent Smith 2', 'user'),
   ('admin1@gmail.com', '12093', '$2b$10$7.mXiBV57u05riDryVUTK.E9DqYtFMUM9i3HS6P2VTmaULAl1v8fm', 'Admin User 1', 'admin'),
   ('admin2@gmail.com', '12094', '$2b$10$7.mXiBV57u05riDryVUTK.E9DqYtFMUM9i3HS6P2VTmaULAl1v8fm', 'Admin User 2', 'admin')
-ON CONFLICT (email) DO NOTHING;
+ON CONFLICT DO NOTHING;
