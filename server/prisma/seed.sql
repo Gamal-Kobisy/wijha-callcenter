dd6083974bda228d14fd3402e0b263c3ec4e0aa3
Gamal Kobisy <gamal@kobisy.dev>
2026-09-05 08:51:43 +0300

    Correct DB path

--- server/prisma/seed.sql
+++ server/prisma/seed.sql
@@ -4,4 +4,4 @@ VALUES
   ('agent1@gmail.com', '123-456-7891', '$2a$12$iTAykiIcy2J.pDxlrffJ1ODdHFi.vDARoL5BI2qpgStbqYylLJyEq', 'Agent Smith 1', 'user'),
   ('agent2@gmail.com', '124-456-7890', '$2b$10$eUnSYctw2bA5lcDxvHyqOu4Bemi6NIQv0wTUJcIsr53t7fKi0SlOy', 'Agent Smith 2', 'user'),
   ('admin1@gmail.com', '120-342-4235', '$2a$12$bAQZI.kF9xu2mh7aNQJ6Z.96wdjWbr1cNxUalOVnSd5/Ds4cfSzkm', 'Admin User 1', 'admin')
-ON CONFLICT (email) DO NOT
+ON CONFLICT (email) DO NOTHING;
